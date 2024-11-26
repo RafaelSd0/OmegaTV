07e0567a86116653a5bfb50e2d8481ee7ad44233
RafaelSd0 <player08901@gmail.com>
2024-11-26 17:11:07 -0300

    header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
-
+import { Component} from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+import { InputTextModule } from 'primeng/inputtext';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ ButtonModule ,ToolbarModule, InputTextModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
