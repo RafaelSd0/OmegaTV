754335af4998e8e44edd976fdb11933cdd6686a9
RafaelSd0 <player08901@gmail.com>
2024-11-26 17:38:00 -0300

    rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'video',
+    loadComponent: () => import('./pages/video-page/video-page.component').then(m => m.VideoPageComponent)
+  },
+];
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,52 @@
-import { Component} from '@angular/core';
+import { Component, Inject} from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { ToolbarModule } from 'primeng/toolbar';
 import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { AuthService, User } from '@auth0/auth0-angular';
+import { RouterLinkActive } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ ButtonModule ,ToolbarModule, InputTextModule],
+  imports: [ ButtonModule ,ToolbarModule, InputTextModule, CommonModule, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  menuAberto: boolean = false;
+  profile!: User | null | undefined;
 
+  constructor(public auth: AuthService, @Inject(DOCUMENT) private document: Document) {}
+
+  ngOnInit(): void {
+
+    this.auth.user$.subscribe((profile) => {
+
+    this.profile = profile;
+
+    });
+  }
+
+  login() {
+    this.auth.loginWithRedirect();
+  }
+
+  logout() {
+    this.auth.logout({
+      logoutParams: {
+        returnTo: this.document.location.origin
+      }
+    });
+  }
+
+  //Mostrar menu para usuarios do Mobile
+  mostrarMenu(): void {
+    const menu = this.document.querySelector('#menu') as HTMLDivElement | null;
+    if (!menu) return;
+
+    this.menuAberto = !this.menuAberto;
+    menu.classList.toggle('hidden', !this.menuAberto);
+    menu.classList.toggle('flex', this.menuAberto);
+  }
 }
