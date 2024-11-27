f69b1086f0b66ba34f751705317af582dd02fde2
RafaelSd0 <player08901@gmail.com>
2024-11-26 23:44:27 -0300

    navegação concluida

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -16,9 +16,18 @@ export const appConfig: ApplicationConfig = {
       authorizationParams: {
 
       redirect_uri: window.location.origin,
+      audience: 'https://dev-h1akqwp5consne5a.us.auth0.com/api/v2/',
 
-  },
+      scope: 'openid profile email offline_access',
 
-  }),
-  ]
-};
+      },
+
+      useRefreshTokens: true,
+
+      cacheLocation: 'localstorage',
+
+      }),
+
+      ],
+
+     };
