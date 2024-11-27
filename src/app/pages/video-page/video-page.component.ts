548b57b84001d64776862961076d177d23aea788
RafaelSd0 <player08901@gmail.com>
2024-11-27 04:59:22 -0300

    rsponsive designe

--- src/app/pages/video-page/video-page.component.ts
+++ src/app/pages/video-page/video-page.component.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
-import { NavegationService } from '../../navegation.service';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavegationService } from '../../navegation.service';
 
 @Component({
   selector: 'app-video-page',
@@ -10,15 +9,15 @@ import { CommonModule } from '@angular/common';
   templateUrl: './video-page.component.html',
   styleUrls: ['./video-page.component.css']
 })
-
 export class VideoPageComponent implements OnInit {
   video: any;
 
-  constructor(private navegationService: NavegationService) {}
+  constructor(private navegationService: NavegationService,) {}
 
-  // Método correto do ciclo de vida
   ngOnInit(): void {
     this.video = this.navegationService.getVideo();
-    console.log("video: ",this.video);
+
   }
+
+
 }
